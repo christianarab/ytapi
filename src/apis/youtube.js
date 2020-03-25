b7a94cc9a232453cfb1d32a98c9009776680d40f
christian <rewindforward@gmail.com>
2020-03-25 10:24:59 -0400

    this is my test commit

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'YOUTUBE_API_KEY_REMOVED';
+
+export const baseParams = {
+  part: "snippet",
+  maxResults: 5,
+  key: KEY
+};
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3"
+});
