b5ae36cd5df9028da30068990b0b3c959beaefe5
christian <rewindforward@gmail.com>
2020-03-26 14:09:34 -0400

    changing results list bg white smaller thmbs

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem';
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map( video => { return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video}/>});
 
-  return (<div className="ui relaxed divided list">{ renderedList }</div>)
+  return (<div className="ui relaxed divided list" style={{backgroundColor:'white'}}>{ renderedList }</div>)
 };
 
 export default VideoList;
